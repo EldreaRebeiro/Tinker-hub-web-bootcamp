555017f93f088d68e4522220dc0adc87fe71bacf
EldreaRebeiro <84325024+EldreaRebeiro@users.noreply.github.com>
2022-02-04 17:53:23 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,15 @@
+function changeTopink() {
+  document.body.style.backgroundColor = "pink";
+}
+
+function changeToyellow() {
+  document.body.style.backgroundColor = "yellow";
+}
+
+function changeTopurple() {
+  document.body.style.backgroundColor = "purple";
+}
+
+function changeTowhite() {
+  document.body.style.backgroundColor = "white";
+}
